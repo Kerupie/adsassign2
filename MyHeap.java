571f3647cea0bc11f2287d3cbcddf59e18e53e85
Kerupie <97594360+Kerupie@users.noreply.github.com>
2022-04-23 23:25:18 +0600

    Update MyHeap.java

--- MyHeap.java
+++ MyHeap.java
@@ -1,5 +1,30 @@
-import java.util.LinkedList;
-
+import java.util.ArrayList;
 public class MyHeap<T> {
+private ArrayList<T> Heap;
+    public MyHeap(){
+        Heap = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+
+        return (Heap.size() == 0);
+    }
+
+    public int size() {
+
+        return Heap.size();
+    }
+
+    private int parent(int position) {
+        return (position - 1) / 2;
+    }
+
+    private int leftChild(int position) {
+        return (2 * position) + 1;
+    }
+
+    private int rightChild(int position) {
+        return (2 * position) + 2;
+    }
 
 }
